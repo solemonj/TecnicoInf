326d8defecb29de9b19bdd8d94836c3dbf9e69e4
Solemon <104238105+solemonj@users.noreply.github.com>
2022-06-01 12:51:38 -0300

    Primeiros códigos Java

---
+++ Somador.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigos;
+
+/**
+ *
+ * @author Usuário
+ */
+public class Somador {
+    
+    public static void main(String[] args){
+        
+        Integer numero1 = 10;
+        Integer numero2 = 5;
+        Integer resultado;
+        
+        resultado = numero1 + numero2;
+        System.out.println("Resultado = " + resultado);
+    }
+    
+    
+    
+}
